a360300f0e61c7ae49a563351911554436c9e74a
Lewis Clarke <lclarke159@gmail.com>
2023-06-19 19:21:27 +0100

    completed exercise10

--- Exercise10/bank_pin_getpass.py
+++ Exercise10/bank_pin_getpass.py
@@ -1,18 +1,19 @@
 # High-street bank mechanism for checking a PIN.
 
 # Correct pin number.
+import getpass
 correct_pin = "0000"
 
 # Set starting number of attempts to 0
 attempts = 0
-# The user is allowed 3 attempts to enter the correct PIN.
+# The user is allowed this number of attempts to enter the correct PIN.
 allowed_attempts = 3
 
 # Loop until the user enters the correct PIN or has exhausted all attempts (allowed_attempts).
 while attempts < allowed_attempts:
 
     # Enter hidden PIN.
-    supplied_pin = input("Enter your PIN: ")
+    supplied_pin = getpass.getpass("Enter your PIN: ")
 
     # Check if the PIN is correct.
     if supplied_pin == correct_pin:
@@ -23,13 +24,13 @@ while attempts < allowed_attempts:
         break
 
     else:
-        # Changes attempts to singular attempt. Could also add last chance warning
+        # changes attempts to singular attempt remaining. Could also add last chance warning
         if attempts + 1 == allowed_attempts - 1:
             print("Attempt {} - you entered an incorrect PIN. You have {} attempt remaining."
                   .format(attempts + 1, allowed_attempts - 1 - attempts))
             attempts += 1
         else:
-            # {} with .format to inform how many attempts used / remaining
+        # {} with .format to inform how many attempts used / remaining
             print("Attempt {} - you entered an incorrect PIN. You have {} attempts remaining."
               .format(attempts + 1, allowed_attempts - 1 - attempts))
         # Add 1 to the number of attempts.
