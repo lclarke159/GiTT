36bee787cf41fa910b3cbcfa75046acf7f30a0df
Lewis Clarke <lclarke159@gmail.com>
2023-07-12 10:19:34 +0100

    added ascii to Ex14

--- Exercise14/rps_function_5.py
+++ Exercise14/rps_function_5.py
@@ -3,13 +3,12 @@ import random
 
 #  Uses function for player selection, computer selection and to play the game.
 #  Includes scoreboard for keeping track of scores
-
-# TODO Invalid selection gives message "None" instead of "Make a valid selection!!"
+# includes ascii dict to make more user-friendly / engaging
 
 def player():
     user_choice = input("Choose (R)ock, (P)aper or (S)cissors and press Enter: ").upper()
     if user_choice not in ["R", "P", "S"]:
-        return "Make a valid selection!!"
+        return "Make a valid selection2!!"
     else:
         return user_choice
 
@@ -30,34 +29,63 @@ def play(player_choice, comp_choice):
         "P": "Paper",
         "S": "Scissors"
     }
+
+    ascii_dict = {
+        "R": """ ROCK!
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+""",
+        "P": """ PAPER!
+     _______
+---'    ____)____
+           ______)
+          _______)
+         _______)
+---.__________)
+""",
+        "S": """ SCISSORS!
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+"""
+    }
+
     loses_to = {
         "R": "S",
         "P": "R",
         "S": "P",
     }
     if player_choice == comp_choice:
-        print(f"Computer chose {selection_dictionary[computer()]}, it's a tie!")
+        print(f"Computer chose... {ascii_dict[comp_choice]} it's a tie!")
         return "Have another go!!"
     elif player_choice == "R" and comp_choice == "S":
-        print("Rock crushes scissors - you win!!")
+        print(f"Computer chose... {ascii_dict[comp_choice]} Rock crushes scissors - you win!!")
         return "Well done!!"
     elif player_choice == "P" and comp_choice == "R":
-        print("Paper covers rock - you win!!")
+        print(f"Computer chose... {ascii_dict[comp_choice]} Paper covers rock - you win!!")
         return "Well done!!"
     elif player_choice == "S" and comp_choice == "P":
-        print("Scissors cut paper - you win!!")
+        print(f"Computer chose... {ascii_dict[comp_choice]} Scissors cut paper - you win!!")
         return "Well done!!"
     elif player_choice == "R" and comp_choice == "P":
-        print("Paper covers rock - you lose!!")
+        print(f"Computer chose... {ascii_dict[comp_choice]} Paper covers rock - you lose!!")
         return "Unlucky!!"
     elif player_choice == "P" and comp_choice == "S":
-        print("Scissors cut paper - you lose!!")
+        print(f"Computer chose... {ascii_dict[comp_choice]} Scissors cut paper - you lose!!")
         return "Unlucky!!"
     elif player_choice == "S" and comp_choice == "R":
-        print("Rock crushes scissors - you lose!!")
+        print(f"Computer chose... {ascii_dict[comp_choice]} Rock crushes scissors - you lose!!")
         return "Unlucky!!"
-    else:  # Required or no selection results in 'none' being printed
+    else:                                                    # Required or no selection results in 'none' being printed
         return "Please make a valid selection!!"
+
 
 
 computer_score = 0
@@ -70,4 +98,8 @@ while True:
         player_score += 1
     elif result == "Unlucky!!":
         computer_score += 1
+    # else:
+    #     print("Make a valid selection4!!")
     print("The score is", computer_score, "-", player_score)
+
+
